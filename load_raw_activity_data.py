8201c7082baa71c08b61b98b40c83d1a48eff364
Zsombor Szabo <zsom.szabo@gmail.com>
2024-02-21 12:57:12 +0100

    RAWPREPROC. Raw preprocessing file (be to new stage)

---
+++ load_raw_activity_data.py
@@ -0,0 +1,25 @@
+from sklearn.preprocessing import StandardScaler
+from analyses.data_preprocessing import get_area_responses
+from utils.download_allen import cache_allen
+from utils.data_io import save_pickle
+import yaml
+
+# Load parameters
+params = yaml.safe_load(open('params.yaml'))['preprocess']
+
+# An arbitrary session from the Allen Neuropixel dataset
+session_id = 1064644573  # 1052533639
+cache = cache_allen()
+session = cache.get_ecephys_session(ecephys_session_id=session_id)
+
+for area in params['areas']:
+
+    # Get the responses for the area
+    full_activity = get_area_responses(
+        session, area, session_block=params['stimulus-block'], log=False)
+
+    print(full_activity.shape)
+
+    # Save the residual activity
+    save_pickle(full_activity,
+                f'{params["stimulus-block"]}_block_{area}-activity', path='data/raw-area-responses')
