b4c362ee8a9c38563327760b771ac00ae7235b33
Zsombor Szabo <zsom.szabo@gmail.com>
2024-04-09 10:51:59 +0200

    Crosstime time series lag session ...84 bottom-up smooth image by bilinear interpolation

--- crosstime-plot.py
+++ crosstime-plot.py
@@ -20,8 +20,8 @@ timeseries = np.array(search['lag'])
 # Add the first timepoint to each element in timeseries
 timeseries = timeseries + search['timepoints'][0]
 
-# Plot the matrix. colormap do not use white color
-plt.imshow(matrix, cmap='viridis', interpolation='nearest')
+# Plot the matrix. colormap do not use white color. Make the resolution higher.
+plt.imshow(matrix, cmap='viridis', interpolation='bilinear')
 plt.xticks(range(timeseries.shape[0]), timeseries)
 plt.yticks(range(timeseries.shape[0]), timeseries)
 plt.colorbar()
