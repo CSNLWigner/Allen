283794859019b5c4620193447a0f62d8c740353e
szabo.zsombor <szabo.zsombor@m3.rmki.kfki.hu>
2024-04-29 12:05:27 +0200

    TD_param_search_GEN3 session 1099598937

---
+++ param-search-crosstime-test.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# TD search
+./runcode.sh TD_param-search_GEN3 session $1 >> GEN3.txt
+
+# sort
+
+
+# clear GEN3.tyt
+
+# BU search
+
+# sort
+
+# TD crosstest
+
+# cp outdir
+
+# BU crosstest
+
+# co outdir
+
+
+
+# Get git name
+git_name="$1"
+
+# Get the param key
+param_key="$2"
+
+# Check the initial value of the param
+if ! grep -q "$param_key: 0" params.yaml; then
+    echo "Error: The initial value of $param_key should be 0 in params.yaml"
+    exit 1
+fi
+
+# Get the param value list (Internal Field Separator is comma, then read by read only into array named XY from the parameter
+IFS=',' read -r -a param_value_list <<< "$3"
+
+# Loop over the parameters
+for param_value in "${param_value_list[@]}"
+do
+    # Print the informations
+    echo "$git_name $param_key $param_value"
+
+    # Add the param key
+    new_param="$param_key: $param_value"
+    old_param="$param_key: 0"
+
+    # Modify the parameters in params.yaml
+    sed -i "s/$old_param/$new_param/g" params.yaml
+
+    # Run code
+    dvc repro
+
+    # Git
+    git add .
+    git commit -m "$git_name $param_key $param_value"
+
+    # Reset the parameters to avoid conflicts in the next iteration
+    sed -i "s/$new_param/$old_param/g" params.yaml
+
+    # Print current date and time
+    date
+done
