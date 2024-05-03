34a7c4397890a9b8f289bb8682cb75b4b4f45e57
Zsombor Szabo <zsom.szabo@gmail.com>
2024-05-03 12:10:48 +0200

    SAVESLICES.

---
+++ save-slices.py
@@ -0,0 +1,21 @@
+
+'''
+This is instead of plot section of the dvc pipeline
+'''
+
+# Import the necessary libraries
+from utils.data_io import load_pickle, save_pickle
+import yaml
+
+# Load the params
+session = yaml.safe_load(open('params.yaml'))['load']['session']
+rrr = yaml.safe_load(open('params.yaml'))['rrr']
+
+# Define the prediction direction
+prediction_direction = 'top-down' if rrr['predictor'] == 'VISl' else 'bottom-up'
+
+# Load the results from dir results
+results = load_pickle('cross-time-RRR', path='results')
+
+# Save the results in a different loction
+save_pickle(results, f'{prediction_direction}_cross-time-RRR_{session}', path='cache')
