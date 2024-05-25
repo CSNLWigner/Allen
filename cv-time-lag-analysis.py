4daac2b06decb09c4d5ce441106c47eba318e7dd
Zsombor Szabo <zsom.szabo@gmail.com>
2024-05-25 17:07:31 +0200

    Transient for nargothrond-m3 debug

--- cv-time-lag-analysis.py
+++ cv-time-lag-analysis.py
@@ -5,6 +5,7 @@ import yaml
 from analyses.data_preprocessing import (get_area_responses,
                                          preprocess_area_responses)
 from utils.data_io import load_pickle, save_pickle
+from utils.debug import ic, debug, hasharr
 
 # Get the arguments
 opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
@@ -22,9 +23,11 @@ main_params = yaml.safe_load(open('params.yaml'))['rrr-param-search']
 # Load the activity
 full_activity_predictor = load_pickle(f'{load["stimulus-block"]}_block_{rrr_params["predictor"]}-activity', path='data/raw-area-responses')
 full_activity_target    = load_pickle(f'{load["stimulus-block"]}_block_{rrr_params["target"]}-activity', path='data/raw-area-responses')
-    
+ic(full_activity_predictor.shape, full_activity_target.shape)
+
 # Get the image names
 image_names = load_pickle(f'{load["stimulus-block"]}_block_image-names', path='data/stimulus-presentations')
+ic(hasharr(image_names))
 
 # Import utile functions
 import numpy as np
@@ -37,6 +40,9 @@ cv = main_params['cv']
 time_lag = main_params['lag']
 rank = main_params['rank']
 timepoints = main_params['timepoints'] # ms
+
+cv, rank, timepoints, time_lag = [4], [30], [50], [0]
+
 timepoint_indices = [int(t / preproc['step-size'] / 1000) for t in timepoints]
 
 # Some calculations
@@ -60,7 +66,9 @@ def calculate_something():
     for j, lag in enumerate(time_lag):
         
         # Preprocess the area responses
+        ic(hasharr(full_activity_predictor))
         predictor = preprocess_area_responses(full_activity_predictor, image_names)
+        ic(hasharr(predictor))
         target = preprocess_area_responses(full_activity_target, image_names)
         
         # Move the activity of V2 back in time by the actual time lag
@@ -71,9 +79,11 @@ def calculate_something():
                 for t, time in zip(timepoint_indices, timepoints):
                     
                     # Reduced Rank Regression
-                    if log: print(f'Cross-validation: {c}, Time lag: {lag}')
                     # result = RRRR(V1.mean(axis=0), V2.mean(axis=0), params['rank'], cv=c) # cross-time RRRR
-                    result = RRRR(predictor[:,:,t].T, lagged_target[:,:,t].T, rank=r, cv=c)
+                    
+                    debug(predictor[:, :, t])
+                    result = RRRR(predictor[:,:,t].T, lagged_target[:,:,t].T, rank=r, cv=c, log=True)
+                    ic('cv', c, 'lag', lag, 'rank', r, 'time', time)
                     
                     # Save the result averaged over the folds
                     results[i, j, k, t] = result['test_score'].mean()
@@ -95,9 +105,10 @@ print(f'Calculating something')
 result = calculate_something()
 
 print('result.shape:', result.shape)
+# debug(result)
 
 # Save the results
-save_pickle(result, f'CV-lag-time')
+# save_pickle(result, f'CV-lag-time')
 
 # Get the maximum
 max = np.nanmax(result).round(3)
@@ -109,7 +120,7 @@ max_idx = np.unravel_index(np.nanargmax(result), result.shape)
 print(f'maximum value({max}) is at time={timepoints[max_idx[3]]} s, cv={cv[max_idx[0]]} fold, lag={time_lag[max_idx[1]]} ms, and rank={rank[max_idx[2]]}')
 
 # Append the maximum value and the corresponding parameters to a csv file
-with open('best-rrr-params.csv', 'a') as f:
-    f.write(f'{load["session"]},{timepoints[max_idx[3]]},{max},{cv[max_idx[0]]},{time_lag[max_idx[1]]},{rank[max_idx[2]]}\n')
+# with open('best-rrr-params.csv', 'a') as f:
+#     f.write(f'{load["session"]},{timepoints[max_idx[3]]},{max},{cv[max_idx[0]]},{time_lag[max_idx[1]]},{rank[max_idx[2]]}\n')
 
 
