4ab808c0d1af55aef066a635092e125f17cb09af
Zsombor Szabo <zsom.szabo@gmail.com>
2024-05-29 11:56:39 +0200

    rerun crosstime analysis in multiple-timeslices.py

--- multiple-timeslices.py
+++ multiple-timeslices.py
@@ -1,33 +1,50 @@
 
-from matplotlib import pyplot as plt
-import numpy as np
-import yaml
-from analyses.rrr_time_slice import bidirectional_time_slice
-from utils.data_io import load_pickle, save_fig, save_pickle
-from utils.megaplot import megaplot
-from utils import plots
 import sys
 
+import numpy as np
+import yaml
+from matplotlib import pyplot as plt
+
+from analyses.rrr import crosstime_analysis
+from analyses.rrr_time_slice import bidirectional_time_slice
+from utils import plots
+from utils.data_io import load_csv, load_pickle, save_fig, save_pickle
+from utils.megaplot import megaplot
 from utils.utils import printProgressBar
 
 # Get the parameters from the command line
-crosstime_path = sys.argv[1]
+# crosstime_path = sys.argv[1]
 
 # Import params
 load = yaml.safe_load(open("params.yaml"))["load"]
 preprocess = yaml.safe_load(open("params.yaml"))["preprocess"]
-best_params = yaml.safe_load(open("params.yaml"))["best-rrr-params"]
-params = yaml.safe_load(open("params.yaml"))["rrr-time-slice"]
+timeslice = yaml.safe_load(open("params.yaml"))["rrr-time-slice"]
+crosstime = yaml.safe_load(open("params.yaml"))["crosstime"]
+
+# Get sessions-params.csv
+sessions_params = load_csv("sessions-params") # columns: session, predictor, target, direction, r2, time, cv, lag, rank
+
+# Get the rows for the current session
+session_params = sessions_params[sessions_params['session'] == load['session']]
+print(session_params)
+
+# MARK: - Load data
 
 # Load raw data
+raw_V1 = load_pickle(
+    f"{load['stimulus-block']}_block_VISp-activity", path="data/raw-area-responses")
+raw_LM = load_pickle(
+    f"{load['stimulus-block']}_block_VISl-activity", path="data/raw-area-responses")
 V1 = load_pickle(
     f"{load['stimulus-block']}_block_VISp-activity", path="data/area-responses")
 LM = load_pickle(
     f"{load['stimulus-block']}_block_VISl-activity", path="data/area-responses")
+stimuli = load_pickle(
+    f"{load['stimulus-block']}_block_image-names", path="data/stimulus-presentations")
 
 # Define the parameters
 session = load['session']
-predictor_times = params['predictor-time']
+predictor_times = timeslice['predictor-time']
 timepoints = np.arange(0, preprocess['stimulus-duration'], preprocess['step-size'])  # in seconds
 names = {
     'V1': {
@@ -59,18 +76,31 @@ target:V1 | V1 -> V1     | LM -> V1     | slice  | slice  | slice  | slice  | sl
 target:LM | V1 -> LM     | LM -> LM     | slice  | slice  | slice  | slice  | slice  | slice  |
 '''
 
+# MARK: - Crosstime analysis
+
 # The first two columns are the predictor areas
 # The two rows of the first to columns are the target areas
-for i, predictor in zip([0, 1], ['V1', 'LM']): # ROWS
-    for j, target in zip([0, 1], ['V1', 'LM']): # COLUMNS
+for i, predictor, predictor_activity in zip([0, 1], ['V1', 'LM'], [raw_V1, raw_LM]): # ROWS
+    for j, target, target_activity in zip([0, 1], ['V1', 'LM'], [raw_V1, raw_LM]): # COLUMNS
+        
+        # Extract the session parameters
+        if predictor == 'V1':
+            session_key = 'bottom-up'
+        elif predictor == 'LM':
+            session_key = 'top-down'
+        direction_params = session_params[session_params['direction'] == session_key]
+        cv = direction_params['cv'].values[0]
+        rank = direction_params['rank'].values[0]
         
         # Load the crosstime results from cache/*.pickle
-        results = load_pickle(f"{names[predictor][target]}_cross-time-RRR_{session}", path=crosstime_path)
-        # results = load_pickle("cross-time-RRR", path="results")
+        # results = load_pickle(f"{names[predictor][target]}_cross-time-RRR_{session}", path=crosstime_path)
+        
+        # Calculate crosstime analysis
+        # results = crosstime_analysis(predictor_activity, target_activity, stimuli, cv, rank, scaling_factor=crosstime['scaling-factor'])
         
         # Plot the results
         ax = plot[i, j]
-        plots.crosstime_RRR(ax, results, predictor, target, timepoints[timepoints < 0.200])
+        # plots.crosstime_RRR(ax, results, predictor, target, timepoints[timepoints < 0.200])
         
         # Blue and orange lines on the cross-time plots
         if predictor == 'V1' and target == 'LM':
@@ -79,6 +109,8 @@ for i, predictor in zip([0, 1], ['V1', 'LM']): # ROWS
         if predictor == 'LM' and target == 'V1':
             for predictor_time in predictor_times:
                 ax.axhline(y=predictor_time, color=top_down_color, linestyle='-')
+
+# MARK: - Time-slice analysis
                 
 printProgressBar(0, len(predictor_times), prefix = 'RRR analysis:', length = 30)
                 
@@ -87,7 +119,7 @@ for i, predictor_time in enumerate(predictor_times):
     # Get the results from the time-slice analysis
     # results = load_pickle(f"{session}_time-slice_{predictor_time}", path="data/time-slices")
     # results = load_pickle("rrr-time-slice", path="results")
-    results = bidirectional_time_slice(load['session'], V1, LM, best_params, predictor_time)
+    results = bidirectional_time_slice(V1, LM, session_params, predictor_time)
     
     # Plot the results
     ax = plot[:, 2+i]
