45540acabccf4f8bd011d39c9d331e5a4b54c1f5
Zsombor Szabo <zsom.szabo@gmail.com>
2024-06-18 12:22:21 +0200

    fixed vmin-vmax in nan cases

--- layer-interaction-plot.py
+++ layer-interaction-plot.py
@@ -1,7 +1,4 @@
 
-
-
-
 import numpy as np
 import yaml
 
@@ -15,11 +12,16 @@ timepoints = np.arange(0, preprocess['stimulus-duration'], preprocess['step-size
 
 # for direction in ['bottom-up', 'top-down']:
 for originArea, targetArea in zip(['V1', 'LM'], ['LM', 'V1']):
+    print(f"Plotting {originArea} to {targetArea}")
     
     # Results
     results = load_pickle(f"layer-interaction_{originArea}-to-{targetArea}")
     output_layers = list(results.keys())
     input_layers = list(results[output_layers[0]].keys())
+    
+    # Sort the layers by their number in ascending order
+    output_layers = sorted(output_layers)
+    input_layers = sorted(input_layers)
     
     # Get the minimum and maximum values for the colorbar across all plots
     vmin = np.inf
@@ -27,8 +29,9 @@ for originArea, targetArea in zip(['V1', 'LM'], ['LM', 'V1']):
     for y, output in enumerate(results.keys()):
         for x, input in enumerate(results[output].keys()):
             result = results[output][input]
-            vmin = min(vmin, result.min())
-            vmax = max(vmax, result.max())
+            vmin = min(vmin, np.nanmin(result))
+            vmax = max(vmax, np.nanmax(result))
+    print(f"vmin: {vmin}, vmax: {vmax}")
     
     # Plot
     plot = megaplot(nrows=len(output_layers), ncols=len(input_layers), title=f"{originArea} to {targetArea}")
