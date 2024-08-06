be70bd0438a9a27a4908b00371b429e34d5b7678
Zsombor Szabo <zsom.szabo@gmail.com>
2024-08-06 16:52:06 +0200

    adjusted layer rank analysis to be able to save r2

--- analyses/layer_rank.py
+++ analyses/layer_rank.py
@@ -8,9 +8,16 @@ from analyses.rrr import RRRR
 preproc = yaml.safe_load(open('params.yaml'))['preprocess']
 params = yaml.safe_load(open('params.yaml'))['layer-rank']
 
-def cv_search(predictor, target):
+def cv_search(predictor, target) -> tuple:
     """
     Perform a RRRR search for the best cv.
+    
+    Args:
+        predictor (ndarray): The predictor data array.
+        target (ndarray): The target data array.
+    
+    Returns:
+        tuple: A tuple containing the best cv value and the maximum RRRR score.
     """
 
     # Define the cross-validation, and time
@@ -41,10 +48,10 @@ def cv_search(predictor, target):
     max_r2 = np.nanmax(results).round(3)
     best_cv = np.nanargmax(results)
 
-    return best_cv
+    return best_cv, max_r2
 
 
-def rank_search(predictor, target, cv, log=False) -> int:
+def rank_search(predictor, target, cv, log=False) -> tuple:
     """
     Perform a RRRR search for the best rank.
 
@@ -73,22 +80,24 @@ def rank_search(predictor, target, cv, log=False) -> int:
                       success_log=log) # Switch to False!
 
         # Save the result averaged over the folds
-        results[i] = result['test_score'].mean()
+        results[i] = np.nanmean(result['test_score'])
 
     # Cut off the negative values
     results[results < -0] = np.nan
     
     # If all nan, return nan
     if np.all(np.isnan(results)):
-        return np.nan
+        return np.nan, np.nan
 
     # Get the maximum value (max_r2) and its index (max_idx)
     max_r2 = np.nanmax(results).round(3)
     max_idx = np.nanargmax(results)
+    
+    # Get the rank corresponding to the maximum value
+    max_r2_rank = max_idx * params['stepRank'] + params['minRank']
 
-    return max_idx * params['stepRank'] + params['minRank']
+    return max_r2_rank, max_r2
 
-# Some calculations
 
 
 def calc_ranks(V1_data, LM_data, timepoints, log=False):
@@ -117,7 +126,7 @@ def calc_ranks(V1_data, LM_data, timepoints, log=False):
         sourceLayers = sourceArea['layer-assignments'].unique()
         targetLayers = targetArea['layer-assignments'].unique()
 
-        best_cv = cv_search(sourceArea['activity'], targetArea['activity'])
+        best_cv, _ = cv_search(sourceArea['activity'], targetArea['activity'])
 
         # Iterate over layer combinations
         for output in sourceLayers:
@@ -126,10 +135,10 @@ def calc_ranks(V1_data, LM_data, timepoints, log=False):
                 # Iterate over the time points
                 for t in timepoints:
 
-                    best_rank = rank_search(sourceArea['activity'][:, :, t],
-                                            targetArea['activity'][:, :, t],
-                                            best_cv,
-                                            log=log)
+                    best_rank, r2 = rank_search(sourceArea['activity'][:, :, t],
+                                                targetArea['activity'][:, :, t],
+                                                best_cv,
+                                                log=log)
 
                     # Save the result averaged over the folds
                     results[i, output, input, t] = best_rank
